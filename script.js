1ea73fcc530919d314943371ef59917d6cec7530
Khushboo <khushboodhiman716@gmail.com>
2021-07-29 14:18:51 +0530

    Added the loader and start button

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    document.getElementById('loader').style.display='none'
+  }, 3*1000);
+
+  setTimeout(function() {
+    document.getElementById('start').style.opacity=1
+  }, 3*1000);
