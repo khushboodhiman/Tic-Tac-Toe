034d61879fdd479b5832f452c8b5d7bd49ef5d42
Khushboo <khushboodhiman716@gmail.com>
2021-07-29 18:09:38 +0530

    Created the grid

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ setTimeout(function() {
     document.getElementById('loader').style.display='none'
   }, 3*1000);
 
-  setTimeout(function() {
+setTimeout(function() {
     document.getElementById('start').style.opacity=1
-  }, 3*1000);
+}, 3*1000);
+
+function startGame() {
+  document.getElementById('loader-and-all').style.visibility="hidden";
+  document.getElementById('game').style.visibility="visible";
+}
+
+function toggle() {
+  let turns = document.getElementsByClassName('turn');
+  for (turn of turns) {
+    turn.classList.toggle('active');
+  }
+}
+
+function fillValue() {
+  // document.getElementsByClassName("box").value="X";
+  // console.log(this);
+  this.value="X";
+}
